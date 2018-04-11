a38a897ff275dadd8e838580a88c0965343ffac8
Luana <lms@cesar.org.br>
2018-04-11 16:03:43 -0300

    refactor: apply code guidelines rules to span.ts

--- packages/opencensus-core/src/trace/model/span.ts
+++ packages/opencensus-core/src/trace/model/span.ts
@@ -14,68 +14,69 @@
  * limitations under the License.
  */
 
-import { Clock } from '../../internal/clock'
-import { RootSpan } from './rootspan'
-import { debug, randomSpanId } from '../../internal/util'
-import { SpanBaseModel, TraceContext } from '../types/tracetypes'
+import {Clock} from '../../internal/clock';
+import {debug, randomSpanId} from '../../internal/util';
+import {SpanBaseModel, TraceContext} from '../types/tracetypes';
+import {RootSpan} from './rootspan';
 
-
+/**
+ * This class represent a span.
+ */
 export class Span extends SpanBaseModel {
+  private root: RootSpan;
 
-    private root: RootSpan;
-  //  private _parentSpanId: string;
+  /**
+   * Constructs a new Span instance.
+   * @param root
+   */
+  constructor(root: RootSpan) {
+    super();
+    this.root = root;
+  }
 
-    constructor(root: RootSpan) {
-        super()
-        this.root = root;
-    }
+  /** Gets trace id of rootspan. */
+  get traceId(): string {
+    return this.root.traceId;
+  }
 
-    public get traceId(): string {
-        return this.root.traceId;
-    }
+  /** Gets trace id of rootspan. */
+  get parentSpanId(): string {
+    return this.root.id;
+  }
 
-    public get parentSpanId(): string {
-        return this.root.id;
-    }
+  /** Gets trace context of rootspan. */
+  get traceContext(): TraceContext {
+    return {
+      traceId: this.traceId.toString(),
+      spanId: this.id.toString(),
+      options: 1  // always traced
+    };
+  }
 
-    public get traceContext(): TraceContext {
-        return {
-            traceId: this.traceId.toString(),
-            spanId: this.id.toString(),
-            options: 1  // always traced
-        };
-    }
+  /** Starts the span instance. */
+  start() {
+    super.start();
+    debug(
+        'starting span  %o',
+        {traceId: this.traceId, spanId: this.id, name: this.name});
+  }
 
-    public start() {
-        super.start();
-        debug('starting span  %o',
-            {
-                traceId: this.traceId,
-                spanId: this.id,
-                name: this.name
-            })
-    }
+  /** Notifies an end operation on span instance. */
+  private notifyEnd() {
+    this.root.onEndSpan(this);
+  }
 
-    private notifyEnd () {
-        this.root.onEndSpan(this);
-    }
-
-    public end(): void {
-        // if(this.sampler.continue(this.traceId)) {
-            
-            super.end();
-            this.notifyEnd();
-            debug('ending span  %o',
-                {
-                    spanId: this.id,
-                    traceId: this.traceId,
-                    name: this.name,
-                    startTime: this.startTime,
-                    endTime: this.endTime,
-                    duration: this.duration
-                });
-        // }
-    }
-
-
+  /** Ends the span. */
+  end(): void {
+    super.end();
+    this.notifyEnd();
+    debug('ending span  %o', {
+      spanId: this.id,
+      traceId: this.traceId,
+      name: this.name,
+      startTime: this.startTime,
+      endTime: this.endTime,
+      duration: this.duration
+    });
+  }
 }
