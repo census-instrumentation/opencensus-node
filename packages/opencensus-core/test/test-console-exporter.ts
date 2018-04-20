2fe4fe496bee7701dc50165a128a52d07f75a814
Luana <lms@cesar.org.br>
2018-04-20 15:58:46 -0300

    test: add unit tests for consolelog-exporter

---
+++ packages/opencensus-core/test/test-console-exporter.ts
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as assert from 'assert';
+import * as mocha from 'mocha';
+
+import {Buffer} from '../src/exporters/buffer';
+import {ConsoleLogExporter, NoopExporter} from '../src/exporters/consolelog-exporter';
+import {RootSpanImpl} from '../src/trace/model/rootspan';
+import {TracerImpl} from '../src/trace/model/tracer';
+import {RootSpan} from '../src/trace/model/types';
+
+const tracer = new TracerImpl();
+const DEFAULT_BUFFER_SIZE = 3;
+const DEFAULT_BUFFER_TIMEOUT = 20000;  // time in milliseconds
+const defaultBufferConfig = {
+  bufferSize: DEFAULT_BUFFER_SIZE,
+  bufferTimeout: DEFAULT_BUFFER_TIMEOUT
+};
+
+describe('NoopExporter', () => {
+  /** Should do anything when calling onEndSpan() */
+  describe('onEndSpan()', () => {
+    it('should do anything', () => {
+      const exporter = new NoopExporter();
+      const rootSpan = new RootSpanImpl(tracer);
+      exporter.onEndSpan(rootSpan);
+      assert.ok(true);
+    });
+  });
+
+  /** Should do anything when calling publish() */
+  describe('publish()', () => {
+    it('should do anything', () => {
+      const exporter = new NoopExporter();
+      const rootSpan = new RootSpanImpl(tracer);
+      const queue: RootSpan[] = [];
+      queue.push(rootSpan);
+
+      exporter.publish(queue);
+      assert.ok(true);
+    });
+  });
+});
+
+describe('ConsoleLogExporter', () => {
+  /** Should end a span */
+  describe('onEndSpan()', () => {
+    it('should end a span', () => {
+      const exporter = new ConsoleLogExporter(defaultBufferConfig);
+      const rootSpan = new RootSpanImpl(tracer);
+      exporter.onEndSpan(rootSpan);
+      assert.ok(true);
+    });
+  });
+
+  /** Should publish the rootspan in queue */
+  describe('publish()', () => {
+    it('should publish the rootspans in queue', () => {
+      const exporter = new ConsoleLogExporter(defaultBufferConfig);
+      const rootSpan = new RootSpanImpl(tracer);
+      rootSpan.startSpan('name', 'type', rootSpan.traceId);
+      const queue: RootSpan[] = [];
+      queue.push(rootSpan);
+
+      exporter.publish(queue);
+      assert.ok(true);
+    });
+  });
+});
