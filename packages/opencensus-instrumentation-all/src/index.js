def30582e4dd0cdf09108ef77e7b427d9e31cd92
Fabio Silva <fgs@cesar.org.br>
2018-05-31 13:30:13 -0300

    feat: add package instrumentation-all to opencensus-node

--- packages/opencensus-instrumentation-all/src/index.js
+++ packages/opencensus-instrumentation-all/src/index.js
@@ -14,18 +14,4 @@
  * limitations under the License.
  */
 
-/** General purpose constants. */
-const constants = {
-  /** Default maximum size of a buffer. */
-  DEFAULT_BUFFER_SIZE: 100,
-  /** Default max timeout for a buffer before being flushed */
-  DEFAULT_BUFFER_TIMEOUT: 20000,
-  /** Default list of target modules to be instrumented */
-  DEFAULT_INSTRUMENTATION_MODULES: ['http', 'https'],
-  /** Opencensus Scope */
-  OPENCENSUS_SCOPE: '@opencensus',
-  /** Defult prefix for instrumentation modules */
-  DEFAULT_PLUGIN_PACKAGE_NAME_PREFIX: 'instrumentation'
-};
 
-export {constants as Constants};
