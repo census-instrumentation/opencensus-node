10f2b80e37b671180798236d08875ef4e535024b
Alex Amies <alexamies@google.com>
2018-12-10 15:53:33 -0800

    Added client tag key to view

--- examples/stats/web_client_monitoring/app.js
+++ examples/stats/web_client_monitoring/app.js
@@ -54,11 +54,12 @@ const buckets = [0, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80,
                 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000,
                 5000, 10000, 20000, 50000, 100000];
 const tagPhase = "phase";
+const tagClient = "client";
 const latencyView = stats.createView(
   "webmetrics/latency",
   mLatencyMs,
   AggregationType.DISTRIBUTION,
-  [tagPhase],
+  [tagPhase, tagClient],
   "Distribution of latencies",
   buckets
 );
