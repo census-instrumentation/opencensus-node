4d0517ad3b9defed14d130ada8e66b531c7c0ebf
Vignesh <vigneshtdev@gmail.com>
2019-02-15 19:20:37 +0530

    Add Javadoc for all methods. Refactorto directly export functions

--- packages/opencensus-instrumentation-grpc/src/grpc-stats/client-metrics.ts
+++ packages/opencensus-instrumentation-grpc/src/grpc-stats/client-metrics.ts
@@ -1,70 +1,170 @@
+/**
+ * Copyright 2019, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {AggregationType, globalStats, MeasureUnit, View} from '@opencensus/core';
 import {DEFAULT_BYTES_DISTRIBUTION, DEFAULT_MESSAGE_COUNT_DISTRIBUTION, DEFAULT_MILLI_SECONDS_DISTRIBUTION} from './stats-common';
 
-const clientSentMessagesPerRPC = globalStats.createMeasureInt64(
+/**
+ * {@link Measure} for number of messages sent in the RPC.
+ *
+ */
+const GRPC_CLIENT_SENT_MESSAGES_PER_RPC = globalStats.createMeasureInt64(
     'grpc.io/client/sent_messages_per_rpc', MeasureUnit.UNIT,
     'Number of messages sent in the RPC (always 1 for non-streaming RPCs).');
-const clientSentBytesPerRPC = globalStats.createMeasureInt64(
+
+/**
+ * {@link Measure} for total bytes sent across all request messages per RPC.
+ *
+ */
+const GRPC_CLIENT_SENT_BYTES_PER_RPC = globalStats.createMeasureInt64(
     'grpc.io/client/sent_bytes_per_rpc', MeasureUnit.BYTE,
     'Total bytes sent across all request messages per RPC.');
-const clientReceivedMessagesPerRPC = globalStats.createMeasureInt64(
+
+/**
+ * {@link Measure} for number of response messages received per RPC.
+ *
+ */
+const GRPC_CLIENT_RECEIVED_MESSAGES_PER_RPC = globalStats.createMeasureInt64(
     'grpc.io/client/received_messages_per_rpc', MeasureUnit.UNIT,
     'Number of response messages received per RPC (always 1 for non-streaming RPCs).');
-const clientReceivedBytesPerRPC = globalStats.createMeasureInt64(
+
+/**
+ * {@link Measure} for gRPC client uncompressed response bytes.
+ *
+ */
+const GRPC_CLIENT_RECEIVED_BYTES_PER_RPC = globalStats.createMeasureInt64(
     'grpc.io/client/received_bytes_per_rpc', MeasureUnit.BYTE,
     'Total bytes received across all response messages per RPC.');
-const clientRoundtripLatency = globalStats.createMeasureDouble(
+
+/**
+ * {@link Measure} for gRPC client roundtrip latency in milliseconds.
+ *
+ */
+const GRPC_CLIENT_ROUNDTRIP_LATENCY = globalStats.createMeasureDouble(
     'grpc.io/client/roundtrip_latency', MeasureUnit.MS,
     'Time between first byte of request sent to last byte of response received, or terminal error.');
-const clientServerLatency = globalStats.createMeasureDouble(
+
+/**
+ * {@link Measure} for gRPC server latency in milliseconds.
+ *
+ */
+const GRPC_CLIENT_SERVER_LATENCY = globalStats.createMeasureDouble(
     'grpc.io/client/server_latency', MeasureUnit.MS,
     'Propagated from the server and should have the same value as "grpc.io/server/latency');
 
-const methodKey = {
+/**
+ * Tag key that represents a client gRPC method.
+ *
+ * <p>{@link #GRPC_CLIENT_METHOD} is set when an outgoing request starts and is
+ * available in all the recorded metrics.
+ *
+ */
+const GRPC_CLIENT_METHOD = {
   name: 'grpc_client_method'
 };
-const statusKey = {
+
+/**
+ * Tag key that represents a client gRPC canonical status. Refer to
+ * https://github.com/grpc/grpc/blob/master/doc/statuscodes.md.
+ *
+ * <p>{@link #GRPC_CLIENT_STATUS} is set when an outgoing request finishes and
+ * is only available around metrics recorded at the end of the outgoing request.
+ *
+ */
+const GRPC_CLIENT_STATUS = {
   name: 'grpc_client_status'
 };
 
-const clientSentBytesPerRPCView = globalStats.createView(
-    'grpc.io/client/sent_bytes_per_rpc', clientSentBytesPerRPC,
-    AggregationType.DISTRIBUTION, [methodKey],
+/**
+ * {@link View} for client sent bytes per RPC.
+ *
+ */
+const GRPC_CLIENT_SENT_BYTES_PER_RPC_VIEW = globalStats.createView(
+    'grpc.io/client/sent_bytes_per_rpc', GRPC_CLIENT_SENT_BYTES_PER_RPC,
+    AggregationType.DISTRIBUTION, [GRPC_CLIENT_METHOD],
     'Distribution of bytes sent per RPC, by method.',
     DEFAULT_BYTES_DISTRIBUTION);
-const clientReceivedBytesPerRPCView = globalStats.createView(
-    'grpc.io/client/received_bytes_per_rpc', clientReceivedBytesPerRPC,
-    AggregationType.DISTRIBUTION, [methodKey],
+/**
+ * {@link View} for client received bytes per RPC.
+ *
+ */
+const GRPC_CLIENT_RECEIVED_BYTES_PER_RPC_VIEW = globalStats.createView(
+    'grpc.io/client/received_bytes_per_rpc', GRPC_CLIENT_RECEIVED_BYTES_PER_RPC,
+    AggregationType.DISTRIBUTION, [GRPC_CLIENT_METHOD],
     'Distribution of bytes received per RPC, by method.',
     DEFAULT_BYTES_DISTRIBUTION);
-const clientRoundtripLatencyView = globalStats.createView(
-    'grpc.io/client/roundtrip_latency', clientRoundtripLatency,
-    AggregationType.DISTRIBUTION, [methodKey],
+
+/**
+ * {@link View} for client roundtrip latency in milliseconds.
+ *
+ */
+const GRPC_CLIENT_ROUNDTRIP_LATENCY_VIEW = globalStats.createView(
+    'grpc.io/client/roundtrip_latency', GRPC_CLIENT_ROUNDTRIP_LATENCY,
+    AggregationType.DISTRIBUTION, [GRPC_CLIENT_METHOD],
     'Distribution of round-trip latency, by method.',
     DEFAULT_MILLI_SECONDS_DISTRIBUTION);
-const clientCompletedRPCsView = globalStats.createView(
-    'grpc.io/client/completed_rpcs', clientRoundtripLatency,
-    AggregationType.COUNT, [methodKey, statusKey],
+
+/**
+ * {@link View} for completed client RPCs.
+ *
+ * <p>This {@code View} uses measure {@code GRPC_CLIENT_ROUNDTRIP_LATENCY},
+ * since completed RPCs can be inferred over any measure recorded once per RPC
+ * (since it's just a count aggregation over the measure). It would be
+ * unnecessary to use a separate "count" measure.
+ *
+ */
+const GRPC_CLIENT_COMPLETED_RPC_VIEW = globalStats.createView(
+    'grpc.io/client/completed_rpcs', GRPC_CLIENT_ROUNDTRIP_LATENCY,
+    AggregationType.COUNT, [GRPC_CLIENT_METHOD, GRPC_CLIENT_STATUS],
     'Count of RPCs by method and status.', DEFAULT_MILLI_SECONDS_DISTRIBUTION);
-const clientSentMessagesPerRPCView = globalStats.createView(
-    'grpc.io/client/sent_messages_per_rpc', clientSentMessagesPerRPC,
-    AggregationType.DISTRIBUTION, [methodKey],
+
+/**
+ * {@link View} for client sent messages per RPC.
+ *
+ */
+const GRPC_CLIENT_SENT_MESSAGES_PER_RPC_VIEW = globalStats.createView(
+    'grpc.io/client/sent_messages_per_rpc', GRPC_CLIENT_SENT_MESSAGES_PER_RPC,
+    AggregationType.DISTRIBUTION, [GRPC_CLIENT_METHOD],
     'Distribution of sent messages count per RPC, by method.',
     DEFAULT_MESSAGE_COUNT_DISTRIBUTION);
-const clientReceivedMessagesPerRPCView = globalStats.createView(
-    'grpc.io/client/received_messages_per_rpc', clientReceivedMessagesPerRPC,
-    AggregationType.DISTRIBUTION, [methodKey],
+
+/**
+ * {@link View} for client received messages per RPC.
+ *
+ */
+const GRPC_CLIENT_RECEIVED_MESSAGES_PER_RPC_VIEW = globalStats.createView(
+    'grpc.io/client/received_messages_per_rpc',
+    GRPC_CLIENT_RECEIVED_MESSAGES_PER_RPC, AggregationType.DISTRIBUTION,
+    [GRPC_CLIENT_METHOD],
     'Distribution of received messages count per RPC, by method.',
     DEFAULT_MESSAGE_COUNT_DISTRIBUTION);
-const clientServerLatencyView = globalStats.createView(
-    'grpc.io/client/server_latency', clientServerLatency,
-    AggregationType.DISTRIBUTION, [methodKey],
+
+/**
+ * {@link View} for client server latency in milliseconds.
+ *
+ */
+const GRPC_CLIENT_SERVER_LATENCY_VIEW = globalStats.createView(
+    'grpc.io/client/server_latency', GRPC_CLIENT_SERVER_LATENCY,
+    AggregationType.DISTRIBUTION, [GRPC_CLIENT_METHOD],
     'Distribution of server latency as viewed by client, by method.',
     DEFAULT_MILLI_SECONDS_DISTRIBUTION);
 
-export const defaultClientViews: View[] = [
-  clientSentBytesPerRPCView, clientReceivedBytesPerRPCView,
-  clientRoundtripLatencyView, clientCompletedRPCsView,
-  clientSentMessagesPerRPCView, clientReceivedMessagesPerRPCView,
-  clientServerLatencyView
+export const GRPC_BASIC_CLIENT_VIEWS: View[] = [
+  GRPC_CLIENT_SENT_BYTES_PER_RPC_VIEW, GRPC_CLIENT_RECEIVED_BYTES_PER_RPC_VIEW,
+  GRPC_CLIENT_ROUNDTRIP_LATENCY_VIEW, GRPC_CLIENT_COMPLETED_RPC_VIEW,
+  GRPC_CLIENT_SENT_MESSAGES_PER_RPC_VIEW,
+  GRPC_CLIENT_RECEIVED_MESSAGES_PER_RPC_VIEW, GRPC_CLIENT_SERVER_LATENCY_VIEW
 ];
--- packages/opencensus-instrumentation-grpc/src/grpc-stats/stats-common.ts
+++ packages/opencensus-instrumentation-grpc/src/grpc-stats/stats-common.ts
@@ -1,41 +1,63 @@
-import {Stats} from '@opencensus/core';
+/**
+ * Copyright 2019, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import {defaultClientViews} from './client-metrics';
-import {defaultServerViews} from './server-metrics';
+import {globalStats} from '@opencensus/core';
+import {GRPC_BASIC_CLIENT_VIEWS} from './client-metrics';
+import {GRPC_BASIC_SERVER_VIEWS} from './server-metrics';
 
+// Common histogram bucket boundaries for bytes received/sets Views.
 export const DEFAULT_BYTES_DISTRIBUTION: number[] = [
   0, 1024, 2048, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216,
   67108864, 268435456, 1073741824, 4294967296
 ];
+
+// Common histogram bucket boundaries for latency and elapsed-time Views.
 export const DEFAULT_MILLI_SECONDS_DISTRIBUTION: number[] = [
-  0,   0.01, 0.05, 0.1,  0.3,   0.6,   0.8,   1,     2,   3,   4,
-  5,   6,    8,    10,   13,    16,    20,    25,    30,  40,  50,
-  65,  80,   100,  130,  160,   200,   250,   300,   400, 500, 650,
+  0, 0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1, 2, 3, 4,
+  5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50,
+  65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650,
   800, 1000, 2000, 5000, 10000, 20000, 50000, 100000
 ];
+
+// Common histogram bucket boundaries for request/response count Views.
 export const DEFAULT_MESSAGE_COUNT_DISTRIBUTION: number[] = [
   0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
   32768, 65536
 ];
 
 /**
- * Contains the methods to register basic GRPC views.
+ * Method to register all GRPC Views.
  */
-export class RPCViews {
-  static registerAllGrpcViews(stats: Stats) {
-    this.registerClientGrpcBasicViews(stats);
-    this.registerServerGrpcBasicViews(stats);
+export function registerAllGrpcViews() {
+  registerClientGrpcBasicViews();
+  registerServerGrpcBasicViews();
+}
+/**
+ * Method to register just the client GRPC Views.
+ */
+export function registerClientGrpcBasicViews() {
+  for (const GRPC_CLIENT_VIEW of GRPC_BASIC_CLIENT_VIEWS) {
+    globalStats.registerView(GRPC_CLIENT_VIEW);
   }
-
-  static registerClientGrpcBasicViews(stats: Stats) {
-    for (const CLIENT_VIEW of defaultClientViews) {
-      stats.registerView(CLIENT_VIEW);
-    }
-  }
-
-  static registerServerGrpcBasicViews(stats: Stats) {
-    for (const SERVER_VIEW of defaultServerViews) {
-      stats.registerView(SERVER_VIEW);
-    }
+}
+/**
+ * Method to register just the server GRPC Views.
+ */
+export function registerServerGrpcBasicViews() {
+  for (const SERVER_VIEW of GRPC_BASIC_SERVER_VIEWS) {
+    globalStats.registerView(SERVER_VIEW);
   }
 }
