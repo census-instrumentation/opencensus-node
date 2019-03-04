b302bddb4998e66c1b7cb0eac17bfcd7862134c5
Vignesh <vigneshtdev@gmail.com>
2019-03-04 22:57:12 +0530

    Refactor to make use of buffer

--- packages/opencensus-core/src/tags/propagation/binary-serializer.ts
+++ packages/opencensus-core/src/tags/propagation/binary-serializer.ts
@@ -15,25 +15,27 @@
  */
 
 // import {TextDecoder} from 'util';
-import * as VarInt from '../../internal/VarInt';
+import * as VarInt from '../../internal/varint-utils';
 import {TagMap} from '../tag-map';
 import {TagKey, TagValue} from '../types';
 
-export let VERSION_ID = 0;
+export const VERSION_ID = 0;
 export let TAG_FIELD_ID = 0;
 // This size limit only applies to the bytes representing tag keys and values.
 const TAG_MAP_SERIALIZED_SIZE_LIMIT = 8192;
 
 // Serializes a TagMap to the on-the-wire format.
-export function serializeBinary(tagMap: TagMap): Int8Array {
+export function serializeBinary(tagMap: TagMap): Buffer {
+  const result = Buffer.alloc(TAG_MAP_SERIALIZED_SIZE_LIMIT);
   let totalCharacters = 0;
-  const arr: Int8Array = new Int8Array([VERSION_ID]);
+  // const arr: Int8Array = new Int8Array([VERSION_ID]);
+  result.writeInt32BE(VERSION_ID, result.byteLength);
   const tags = tagMap.tags;
   if (tags) {
     tags.forEach((value: TagValue, key: TagKey) => {
       totalCharacters += key.name.length;
-      totalCharacters += value.value.toString().length;
-      encodeTag(key, value, arr);
+      totalCharacters += value.value.length;
+      encodeTag(key, value, result);
     });
   }
   if (totalCharacters > TAG_MAP_SERIALIZED_SIZE_LIMIT) {
@@ -41,28 +43,24 @@ export function serializeBinary(tagMap: TagMap): Int8Array {
         'Size of TagMap exceeds maximum serialized size' +
         TAG_MAP_SERIALIZED_SIZE_LIMIT);
   }
-  return arr;
+  return result;
 }
 
-function encodeTag(tagKey: TagKey, tagValue: TagValue, byteArray: Int8Array) {
-  byteArray = new Int8Array([...byteArray, TAG_FIELD_ID]);
-  encodeString(tagKey.name, byteArray);
-  encodeString(tagValue.value, byteArray);
+function encodeTag(tagKey: TagKey, tagValue: TagValue, buffer: Buffer) {
+  buffer.writeInt32BE(TAG_FIELD_ID, buffer.byteLength);
+  encodeString(tagKey.name, buffer);
+  encodeString(tagValue.value, buffer);
 }
 
-function encodeString(input: string, byteArray: Int8Array) {
-  putVarInt(input.length, byteArray);
-  byteArray = new Int8Array([
-    ...byteArray,
-  ]);
+function encodeString(input: string, buffer: Buffer) {
+  putVarInt(input.length, buffer);
+  buffer.write(input, buffer.byteLength);
 }
 
-function putVarInt(input: number, byteArray: Int8Array) {
+function putVarInt(input: number, buffer: Buffer) {
   const output: Int8Array = new Int8Array(VarInt.varIntSize(input));
   VarInt.putVarInt(input, output, 0);
-  byteArray = new Int8Array([...byteArray, ...output]);
+  output.forEach(byte => {
+    buffer.writeInt32BE(byte, buffer.byteLength);
+  });
 }
-
-// function decodeString(byteArray: Int8Array): string {
-//   return new TextDecoder('utf-8').decode(byteArray);
-// }
