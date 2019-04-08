9a9f92b7d3b3b34386f503374b2ffe1969ea5a52
Peter Marton <pmarton@netflix.com>
2019-04-08 15:43:59 -0700

    test(pluginloader): re-enable tests

--- packages/opencensus-nodejs/test/test-plugin-loader.ts
+++ packages/opencensus-nodejs/test/test-plugin-loader.ts
@@ -48,7 +48,7 @@ describe('Plugin Loader', () => {
   });
 
 
-  describe.skip('PluginLoader', () => {
+  describe('PluginLoader', () => {
     const plugins = PluginLoader.defaultPluginsFromArray(TEST_MODULES);
     const tracer = new CoreTracer();
     tracer.start({logger: log});
