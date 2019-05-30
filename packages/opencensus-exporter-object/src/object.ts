5c13fc2761d4cb6059713c8043c75d927b9ad4b6
Peter Marton <pmarton@netflix.com>
2019-05-30 07:45:28 +0200

    refactor(exporter-object): fix buffer public

--- packages/opencensus-exporter-object/src/object.ts
+++ packages/opencensus-exporter-object/src/object.ts
@@ -19,7 +19,7 @@ import {logger, Logger} from '@opencensus/core';
 
 /** Object Exporter manager class */
 export class ObjectTraceExporter implements Exporter {
-  private buffer: ExporterBuffer;
+  public buffer: ExporterBuffer;
   logger: Logger;
 
   startedSpans: Span[] = [];
