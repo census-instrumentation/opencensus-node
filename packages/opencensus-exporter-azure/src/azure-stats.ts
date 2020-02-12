12bcf3f0a170dc6b202f661c37d245c8e57f574e
jwlong <jwlong@ncsu.edu>
2020-02-12 14:03:03 -0500

    Documented and organized the exporter skeleton.

--- packages/opencensus-exporter-azure/src/azure-stats.ts
+++ packages/opencensus-exporter-azure/src/azure-stats.ts
@@ -3,22 +3,47 @@ import {
     StatsEventListener
 } from '@opencensus/core';
 
+/**
+ * Options for Azure Monitor configuration.
+ */
 export interface AzureStatsExporterOptions extends ExporterConfig {
 
 }
 
+/**
+ * Formats and sends Stats to Azure Monitor.
+ */
 export class AzureStatsExporter implements StatsEventListener {
 
+    /**
+     * Is called whenever a view is registered.
+     * @param view The registered view.
+     */
     onRegisterView(view: import("@opencensus/core").View): void {
         throw new Error("Method not implemented.");
-    }    onRecord(views: import("@opencensus/core").View[], measurement: import("@opencensus/core").Measurement, tags: Map<import("@opencensus/core").TagKey, import("@opencensus/core").TagValue>): void {
+    }    
+    
+    /**
+     * Is called whenever a measure is recorded.
+     * @param views The views related to the measurement
+     * @param measurement The recorded measurement
+     * @param tags The tags to which the value is applied
+     */
+    onRecord(views: import("@opencensus/core").View[], measurement: import("@opencensus/core").Measurement, tags: Map<import("@opencensus/core").TagKey, import("@opencensus/core").TagValue>): void {
         throw new Error("Method not implemented.");
     }
 
+    /**
+     * Creates an Azure Monitor Stats exporter with an AzureStatsExporterOptions.
+     */
     start(): void {
         throw new Error("Method not implemented.");
     }
 
+    /**
+     * Clear the interval timer to stop uploading metrics. It should be called
+     * whenever the exporter is not needed anymore.
+     */
     stop(): void {
         throw new Error("Method not implemented.");
     }
