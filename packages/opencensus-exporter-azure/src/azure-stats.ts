fb74e5e5898f4d69705ec7ffb70c1f975e7def1c
jwlong <jwlong@ncsu.edu>
2020-04-02 12:01:23 -0400

    Isolate aggregation logic into a function

--- packages/opencensus-exporter-azure/src/azure-stats.ts
+++ packages/opencensus-exporter-azure/src/azure-stats.ts
@@ -27,6 +27,19 @@ export interface StatsParams {
     recordedData: { [key: string]: AggregationData[] };
 }
 
+export enum ExportMode {
+    SINGLE_VALUE = 0,
+    BATCH = 1
+}
+
+export enum AggregationMethod {
+    AVERAGE = 0,
+    MIN = 1,
+    MAX = 2,
+    SUM = 3,
+    COUNT = 4
+}
+
 /**
  * Options for Azure Monitor configuration.
  */
@@ -39,6 +52,12 @@ export interface AzureStatsExporterOptions extends ExporterConfig {
     instrumentationKey: string;
 
     /**
+     * If specified, dictates the mode the exporter will function in.
+     * Optional, defaults to ExportMode.SINGLE_VALUE.
+     */
+    exportMode?: ExportMode;
+
+    /**
      * If specified, defines the number of milliseconds between uploading metrics
      * to Azure Monitor. Optional, defaults to 60,000 (1 minute).
      */
@@ -49,6 +68,12 @@ export interface AzureStatsExporterOptions extends ExporterConfig {
      * Azure Monitor metric. Optional.
      */
     prefix?: string;
+
+    /**
+     * The aggregation method if batch exporting is to be used.
+     * Optional, defaults to AggregationMethod.AVERAGE.
+     */
+    aggregationMethod?: AggregationMethod;
 
     /**
      * If specified, this will serve as the logger used by the exporter.
@@ -78,6 +103,8 @@ const AZURE_STATS_EXPORTER_DEFAULTS: AzureStatsExporterOptions = {
     instrumentationKey: 'undefined',
     periodInMillis: 60000,
     prefix: 'OpenCensus',
+    exportMode: ExportMode.SINGLE_VALUE,
+    aggregationMethod: AggregationMethod.AVERAGE,
     logger: logger.logger()
 }
 
@@ -90,12 +117,6 @@ export class AzureStatsExporter implements StatsEventListener {
 
     // Define all other exporter variables.
     private timer: NodeJS.Timer;
-
-    private statsParams: StatsParams = {
-        registeredViews: [],
-        registeredMeasures: [],
-        recordedData: {},
-      };
 
     /**
      * Configures a new Stats Exporter given a set of options.
@@ -122,8 +143,8 @@ export class AzureStatsExporter implements StatsEventListener {
      * @param view The registered view.
      */
     onRegisterView(view: View): void {
-    }    
-    
+    }
+
     /**
      * Is called whenever a measure is recorded.
      * @param views The views related to the measurement.
@@ -131,23 +152,26 @@ export class AzureStatsExporter implements StatsEventListener {
      * @param tags The tags to which the value is applied.
      */
     // Use the App Insights SDK to track this measurement.
-    // TODO: Try to break this out into smaller methods so we can clearly see
-    // the inputs and outputs.
     onRecord(views: View[], measurement: Measurement, tags: Map<TagKey, TagValue>): void {
-        let newMetric: MetricTelemetry = {
-            name: measurement.measure.name,
-            value: measurement.value
-        };
+        if (this.options.exportMode === ExportMode.SINGLE_VALUE) {
+            let newMetric: MetricTelemetry = {
+                name: measurement.measure.name,
+                value: measurement.value
+            };
 
-        this.exportSingleMetric(newMetric);
-        // ApplicationInsights.defaultClient.trackMetric(newMetric);
-        // this.options.logger.debug('Tracked metric: ', newMetric);   
-    }    
+            this.exportSingleMetric(newMetric);
+        }
+    }
 
     /**
      * Creates an Azure Monitor Stats exporter with an AzureStatsExporterOptions.
      */
     start(): void {
+        // Verify that the exporter is running in batch mode.
+        if (!this.verifyBatchMode()) {
+            return;
+        }
+
         // Set a timer using the interval (period) defined in the exporter's options.
         // Each time the timer ticks, export any data that has been tracked by utilizing
         // the exporter's export() function.
@@ -168,6 +192,11 @@ export class AzureStatsExporter implements StatsEventListener {
      * whenever the exporter is not needed anymore.
      */
     stop(): void {
+        // Verify that the exporter is running in batch mode.
+        if (!this.verifyBatchMode()) {
+            return;
+        }
+
         // Stop the timer.
         clearInterval(this.timer);
         this.options.logger.info('Clearing export interval.');
@@ -179,6 +208,11 @@ export class AzureStatsExporter implements StatsEventListener {
      * Polls the Metrics library for all registered metrics and uploads these to Azure Monitor.
      */
     async export() {
+        // Verify that the exporter is running in batch mode.
+        if (!this.verifyBatchMode()) {
+            return;
+        }
+        
         this.options.logger.info('Starting export of metric batch.');
 
         // Collect all of the metrics that will need to be exported in this batch.
@@ -198,13 +232,12 @@ export class AzureStatsExporter implements StatsEventListener {
         // Aggregate each metric before sending them to Azure Monitor.
         // TODO: Aggregate metrics.
         for (const metric of metricList) {
+            let aggregateValue: number;
+            console.log(metric);
             switch (metric.descriptor.type) {
-                case MetricDescriptorType.UNSPECIFIED:
-                    // Log a warning as this type should not be used.
-                    break;
                 case MetricDescriptorType.GAUGE_INT64:
                 case MetricDescriptorType.GAUGE_DOUBLE:
-                    // Aggregate these by (averaging, mode, etc) and uploading just the average
+                    aggregateValue = this.getMetricAggreation<number>(metric);
                     break;
                 case MetricDescriptorType.CUMULATIVE_INT64:
                 case MetricDescriptorType.CUMULATIVE_DOUBLE:
@@ -218,19 +251,49 @@ export class AzureStatsExporter implements StatsEventListener {
                 case MetricDescriptorType.CUMULATIVE_DISTRIBUTION:
                     // Need to look into how Azure monitor accepts distribution values.
                     break;
+                case MetricDescriptorType.UNSPECIFIED:
+                    // Log a warning as this type should not be used.
+                    break;
                 default:
             }
+
+            this.exportSingleMetric({
+                name: metric.descriptor.name,
+                value: aggregateValue
+            });
         }
     }
 
-     /**
-     * Uses the Application Insights SDK to export a given MetricTelemetry object
-     * into Azure Monitor.
-     * @param metric The MetricTelemetry object to export to Azure Monitor.
+    private getMetricAggreation<T>(metric: Metric): T {
+        switch(this.options.aggregationMethod) {
+            case AggregationMethod.AVERAGE:
+                // Metric.TimeSeries is an array of TimeSeries
+                // Each of these has an array of points
+                // Need to collect all the points from each timeseries and aggregate them
+        }
+        return null;
+    }
+
+    /**
+     * Verifies that that exporter is running batch mode. If it is not,
+     * this method will issue a warning log message.
      */
-    private exportSingleMetric(metric: MetricTelemetry){
+    private verifyBatchMode() : boolean {
+        if (this.options.exportMode !== ExportMode.BATCH) {
+            this.options.logger.warn('This exporter is not configured to run in batch mode.');
+            return false;            
+        }
+        return true;
+    }
+
+    /**
+    * Uses the Application Insights SDK to export a given MetricTelemetry object
+    * into Azure Monitor.
+    * @param metric The MetricTelemetry object to export to Azure Monitor.
+    */
+    private exportSingleMetric(metric: MetricTelemetry) {
         ApplicationInsights.defaultClient.trackMetric(metric);
-        this.options.logger.debug('Tracked metric: ',metric); 
+        this.options.logger.debug('Tracked metric: ', metric);
     }
 
     /**
